1047582e6b67ee17728d7260cc2685989fc8531d
Melza-Geno-I <melzageno.i@gmail.com>
2023-12-26 16:55:31 +0530

    added contacts dynamically in contacts tab

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,6 +3,7 @@ import 'remixicon/fonts/remixicon.css'
 import './tabs'
 import Contact from './contacts'
 import { faker } from '@faker-js/faker'
+import '../styles/components/contacts.scss'
 
 let contacts = [];
 for (let i = 0; i< 10; i++){
@@ -17,5 +18,23 @@ for (let i = 0; i< 10; i++){
 
 }
 
-
+// Dynamic contacts in contacts tab
+const contactList = document.getElementById('contact-list');
+let contactHTML ='';
+contacts.forEach(contact =>{
+    contactHTML += `
+        <li class = "contact ${contact.IsOnline? 'online' : 'offline'}">
+                      <div class="profile-img">
+                        <img src="${contact.picture}" alt="">
+                      </div>
+                      <div class="profile-details">
+                        <div class="name">${contact.firstName}</div>
+                        <div class="details">
+                          Lorem ipsum dolor consectetur.
+                        </div>
+                      </div>
+                    </li>
+    `
+})
+contactList.innerHTML = contactHTML
 
