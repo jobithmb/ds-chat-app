f63eaf6c62f136e161a46b4e66ae37f8cf0d8f05
Melza-Geno-I <144795331+Melza-Geno-I@users.noreply.github.com>
2023-12-27 12:34:06 +0530

    Revert "Modified Recent Tab code "

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,12 +1,9 @@
 import '../styles/main.scss'
 import 'remixicon/fonts/remixicon.css'
 import './tabs'
-
-
 import Contact from './contacts'
 import { faker } from '@faker-js/faker'
 import '../styles/components/contacts.scss'
-
 
 let contacts = [];
 for (let i = 0; i< 10; i++){
@@ -15,28 +12,11 @@ for (let i = 0; i< 10; i++){
         faker.person.firstName(),
         faker.person.lastName(),
         faker.image.avatar(),
-        faker.datatype.boolean(0.5)
+        faker.datatype.boolean(0.75)
        )
     )
-}
-const contactList = document.getElementById('contacts-list');
-let ListItems = '';
-contacts.forEach(contact => {
-    ListItems += `
-        <li class="contact ${contact.IsOnline ? 'online' : 'offline' }">
-            <div class="profile-img">
-            <img src="${contact.picture}" alt="Image">
-            </div>
-            <div class="profile-details">
-            <div class="name">${contact.getFullname()}</div>
-            <div class="details">
-                Lorem ipsum dolor, sit amet consectetur.
-            </div>
-            </div>
-        </li>
-    `
-});
 
+}
 
 // Dynamic contacts in contacts tab
 const contactList = document.getElementById('contact-list');
@@ -57,5 +37,4 @@ contacts.forEach(contact =>{
     `
 })
 contactList.innerHTML = contactHTML
-
 
